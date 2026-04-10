77af06c06d664edde0e1f0f142a3dd7826d01f8e
mohdkaifkhann <141579300+mohdkaifkhann@users.noreply.github.com>
2026-04-11 01:56:27 +0530

    Create AddItem1 component for todo items

---
+++ AddItem1.jsx
@@ -0,0 +1,15 @@
+function AddItem1(){
+   let todoName = 'Buy Milk';
+   let todoDate = '4/10/2025';
+ return( <div className="container">
+         <div className="row kg-row">
+    <div className="col-6">{todoName}</div>
+       <div className="col-4">{todoDate}</div>
+    <div className="col-2"> <button type="button" className="btn btn-danger bt">Danger</button>
+    </div>
+  </div>
+ </div>
+         );
+   }
+
+export default AddItem1;
