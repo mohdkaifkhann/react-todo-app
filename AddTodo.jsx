64b83693c05d3d94ac57dd96685e370bff94d732
mohdkaifkhann <141579300+mohdkaifkhann@users.noreply.github.com>
2026-04-11 01:54:24 +0530

    Create AddTodo component for adding todos

---
+++ AddTodo.jsx
@@ -0,0 +1,13 @@
+function AddTodo(){
+   return <div className="container text-center">
+    <div className="row kg-row">
+    <div className="col-6">
+    <input type="text" placeholder="enter todo here"/></div>
+       <div className="col-4">
+       <input type="date"/></div>
+    <div className="col-2">
+  <button type="button" className="btn btn-success bt">Add</button>  </div>
+  </div>
+   </div>
+   }
+export default AddTodo;
